e92f5ac56e6c80717ef011b660b908a8a3095089
Gundars Vasilevskis <Gundars.Vasilevskis@teamviewer.com>
2019-11-07 13:54:33 +0100

    Added title for tables

--- src/Components/Results/Results.tsx
+++ src/Components/Results/Results.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Result } from "../../Interfaces/Results";
-import { Box, Table, TableHead, TableRow, TableCell, TableBody } from "@material-ui/core";
+import { Box, Table, TableHead, TableRow, TableCell, TableBody, Typography } from "@material-ui/core";
 import './ResultsStyles.css';
 
 interface IProps {
@@ -23,34 +23,43 @@ export default class Results extends React.Component<IProps> {
                 <Box className="firstRow" display="flex" flexDirection="row" p={1} m={1} bgcolor="background.paper">
                     {
                         firstRow.map(result => {
-                            return <Box className="dataBox" key={result.name} width="60vh" p={1} bgcolor="grey.300">
-                                <Table aria-label="simple table" className="table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell align="center"><h4>Rank</h4></TableCell>
-                                            <TableCell align="center"><h4>Name</h4></TableCell>
-                                            <TableCell align="center"><h4>Result</h4></TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {result.ranking.map(rank => (
-                                            <TableRow key={rank.name}>
-                                                <TableCell align="center"><p>{rank.rank}</p></TableCell>
-                                                <TableCell align="center"><p>{rank.name}</p></TableCell>
-                                                <TableCell align="center"><p>{rank.result}</p></TableCell>
+                            return <div key={result.name}>
+                                <Typography variant="h6" id="tableTitle">
+                                   {result.name}
+                                </Typography>
+                                <Box className="dataBox"  width="60vh" p={1} bgcolor="grey.300">
+                                    <Table title={result.name} aria-label="simple table" className="table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell align="center"><h4>Rank</h4></TableCell>
+                                                <TableCell align="center"><h4>Name</h4></TableCell>
+                                                <TableCell align="center"><h4>Result</h4></TableCell>
                                             </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </Box>
+                                        </TableHead>
+                                        <TableBody>
+                                            {result.ranking.map(rank => (
+                                                <TableRow key={rank.name}>
+                                                    <TableCell align="center"><p>{rank.rank}</p></TableCell>
+                                                    <TableCell align="center"><p>{rank.name}</p></TableCell>
+                                                    <TableCell align="center"><p>{rank.result}</p></TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </Box>
+                            </div>
                         })
                     }
                 </Box>
                 <Box className="secondRow" display="flex" flexDirection="row" p={1} m={1} bgcolor="background.paper">
                     {
                         secondRow.map(result => {
-                            return <Box className="dataBox" key={result.name} width="60vh" p={1} bgcolor="grey.300">
-                                <Table aria-label="simple table" className="table">
+                            return <div key={result.name}>
+                            <Typography variant="h6" id="tableTitle">
+                               {result.name}
+                            </Typography>
+                            <Box className="dataBox"  width="60vh" p={1} bgcolor="grey.300">
+                                <Table title={result.name} aria-label="simple table" className="table">
                                     <TableHead>
                                         <TableRow>
                                             <TableCell align="center"><h4>Rank</h4></TableCell>
@@ -69,6 +78,7 @@ export default class Results extends React.Component<IProps> {
                                     </TableBody>
                                 </Table>
                             </Box>
+                        </div>
                         })
                     }
                 </Box>
